91b272f4da0f5c255e916c7d407813c84550b414
Jim Hansson <JIHA@powel.com>
2016-08-08 11:21:31 +0200

    Better description of module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,13 @@
 # == Class: outlook_protocol
 #
-# Full description of class outlook_protocol here.
+# Will setup a new url protocol named outlook:
+# this could be used to make links to specific item in outlook, for that you need to find
+# the item id that every outlook item has. this can be found by getting the EntryID from
+# the outlook object in VB script. this id is changed if the object is moved from server
+# to local storage so don't do that! in you script or get the id after the move. 
 #
+# TODO: small VB script that gives you the id in a dialogbox or clipboard.  
+  
 # === Parameters
 #
 # Document parameters here.
