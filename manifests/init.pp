85e841adb2a6abed2297df9422f7277bce5e7266
Jim Hansson <JIHA@powel.com>
2016-08-08 11:22:00 +0200

    Better documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,25 +12,16 @@
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*path*]
+#   The path to the office installation, defaults to
+#   "C:\\Program Files\\Microsoft Office 15\\root\\office15\\"
 #
 # === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
 #
 # === Examples
 #
 #  class { 'outlook_protocol':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#     path  => "C:\Program Files\Microsoft Office 15\root\office15\",
 #  }
 #
 # === Authors
