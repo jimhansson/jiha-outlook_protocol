c0bcaf701f776a4bfa826b465ba3f65245fbfed5
Jim Hansson <jim.hansson@powel.se>
2016-08-08 13:44:32 +0200

    fixed quality issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@
 # to local storage so don't do that! in you script or get the id after the move. 
 #
 # TODO: small VB script that gives you the id in a dialogbox or clipboard.  
-  
+
 # === Parameters
 #
 # Document parameters here.
@@ -32,27 +32,27 @@
 #
 # Copyright 2015 Jim Hansson, unless otherwise noted.
 #
-class outlook_protocol($path = "C:\\Program Files\\Microsoft Office 15\\root\\office15\\") {
+class outlook_protocol($path = 'C:\\Program Files\\Microsoft Office 15\\root\\office15\\') {
   registry::value {
     '(OUTLOOK) outlook-rotocol':
-      key => 'HKEY_CLASSES_ROOT\outlook',
+      key   => 'HKEY_CLASSES_ROOT\outlook',
       value => 'URL Protocol',
-      data => '';
+      data  => '';
     '(OUTLOOK) url-protocol':
-      key => 'HKEY_CLASSES_ROOT\outlook',
+      key   => 'HKEY_CLASSES_ROOT\outlook',
       value => '(default)',
-      data => 'URL:Outlook Folders';
+      data  => 'URL:Outlook Folders';
     '(OUTLOOK) icon':
-      key => 'HKEY_CLASSES_ROOT\outlook\DefaultIcon',
+      key   => 'HKEY_CLASSES_ROOT\outlook\DefaultIcon',
       value => '(default)',
-      data => "${path}\\OUTLOOK.EXE";
+      data  => "${path}\\OUTLOOK.EXE";
     '(OUTLOOK) open':
-      key => 'HKEY_CLASSES_ROOT\outlook\shell',
+      key   => 'HKEY_CLASSES_ROOT\outlook\shell',
       value => '(default)',
-      data => 'open';
+      data  => 'open';
     '(OUTLOOK) command':
-      key => 'HKEY_CLASSES_ROOT\outlook\shell\open\command',
+      key   => 'HKEY_CLASSES_ROOT\outlook\shell\open\command',
       value => '(default)',
-      data => "${path}\\OUTLOOK.EXE /select \"%1\"";
+      data  => "${path}\\OUTLOOK.EXE /select \"%1\"";
   }
 }
